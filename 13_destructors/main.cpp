81d6584dff3b81bfacb02a207cf96e7cb27c50ca
Luis Filipe <learn2b3@gmail.com>
2025-11-01 20:51:25 +0000

    destructor

--- 13_destructors/main.cpp
+++ 13_destructors/main.cpp
@@ -7,9 +7,11 @@ class Number
 {
 	private:
 		double _number;
+		double *_number2;
 		
 	public:
-		Number(double num);
+	Number(double num);
+		Number(double num, double num2);
 		~Number();
 			
 };
@@ -17,19 +19,39 @@ class Number
 Number::Number(double num)
 {
 	_number = num;
+	
+	std::cout << "constructor for number: " << _number << std::endl;
+}
+
+Number::Number(double num, double num2)
+{
+	_number = num;
+	_number2 = (double*) malloc (sizeof(double));
+	*_number2 = num2;
+	
 	std::cout << "constructor for number: " << _number << std::endl;
 }
 
 Number::~Number()
 {
-	std::cout << "Destructor called" << std::endl;
-	std::cout << "Number: " << _number << std::endl;
+	std::cout << "Destructor called for number: " << _number << std::endl;
+	free(_number2);
 }
 
 
+
+void test()
+{
+	Number six(6,1);
+}
+
 int main()
 {
-	Number seven(7);
+	Number seven(7,3);
+	test();
+	
+	Number *five = new Number(5,6);
+	delete(five);
 	
 	return(0);
 }
